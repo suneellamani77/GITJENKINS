1b550c5b39fc916324670b8c16788a89d1ee1955
suneellamani77@gmail.com <suneellamani77@gmail.com>
2018-07-10 15:48:52 +0530

    first push

---
+++ src/test/java/com/sj/jenkins/SampleTest.java
@@ -0,0 +1,17 @@
+package com.sj.jenkins;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class SampleTest {
+	
+	@Test
+	public void sample(){
+		int a=1;
+		int b=2;
+		
+		assertTrue(a+b==3);
+	}
+
+}
